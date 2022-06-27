7b5ae4c41a38850c30b3b8a361bf3e52db55f498
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2022-06-27 01:17:42 +0100

    latest

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ btn.addEventListener('click', function() {
 })
 
 function onSucess(position) {
+    text.innerText = 'Fetching device location...'
     console.log(position);
    let {latitude,longitude} = position.coords
    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=dde2dc70e97d4d28a7460143c4205eaf`)
@@ -19,6 +20,7 @@ function onSucess(position) {
 }
 
 function OnError(error) {
+    console.log(error.code);
     if(error.code == 1) {
         text.innerText = 'You denied the request'
     } else if(error.code == 2) {
